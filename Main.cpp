40edf71c23ba55f5207ea762ad55edf801211671
ASVegaWorks <67890025+ASVegaWorks@users.noreply.github.com>
2020-09-09 17:03:15 -0600

    Main
    Main control with the user menu.

---
+++ Main.cpp
@@ -0,0 +1,101 @@
+#include<iostream>
+#include"NodeMap.h"
+#include"Node.h"
+#include<iomanip>
+#include<sstream>
+#include<vector>
+#include<memory>
+using namespace std;
+
+void PlaceNodeInput(NodeMap &map);
+void Pathfind(NodeMap &map);
+void TurnPathIntoWall(NodeMap &map);
+void ClearInput();
+
+int main() {
+
+	size_t mapSize = 8;
+	static NodeMap &PathMap =  NodeMap::GetInstance(mapSize);
+	PlaceNodeInput(PathMap);
+}
+/*Node types:
+0 = empty space
+1 = start node
+2 = end node
+3 = block node 
+*/
+
+void PlaceNodeInput(NodeMap &map) {
+	size_t  coord1, coord2 = 0;
+	char nodeType = 1;
+	map.DisplayMap();
+	do {
+		if (nodeType == 1) {
+			cout << "Enter the starting node's coordinates seperated by a space: ";
+		}
+		else if (nodeType == 2) {
+			cout << "Enter the ending node's coordinates seperated by a space: ";
+		}
+		coord1, coord2 = 0;
+		cin >> coord2 >> coord1;
+
+		if ( coord1 < map.NodeMapContainer.size() && coord2 < map.NodeMapContainer.size() && nodeType == 1 && map.NodeMapContainer[coord1][coord2]->GetNodeType() == 0) {
+			map.StartNode = make_shared <Node>(coord1, coord2, 'S', nodeType);
+			map.PlaceNode(map.StartNode);
+			map.DisplayMap();
+			cout << "Start node placed at: " << coord1 << "," << coord2 << "\n";
+			nodeType = 2;
+		}
+		else if (coord1 < map.NodeMapContainer.size() && coord2 < map.NodeMapContainer.size() && nodeType == 2 && map.NodeMapContainer[coord1][coord2]->GetNodeType() == 0) {
+			map.EndNode = make_shared <Node>(coord1, coord2, 'E', nodeType);
+			map.PlaceNode(map.EndNode);
+			map.DisplayMap();
+			cout << "End node placed at: " << coord1 << "," << coord2 << "\n";
+			Pathfind(map);
+			nodeType = 1;
+		}
+		else if (coord1 < map.NodeMapContainer.size() && coord2 < map.NodeMapContainer.size() && map.NodeMapContainer[coord1][coord2]->GetNodeType() != 0) {
+			map.DisplayMap();
+			cout << "That space is occupied. \n";
+}
+		else  {
+			map.DisplayMap();
+			cout << "Invalid input. \n"; 
+			ClearInput();
+		}
+	} while ( nodeType  <= 3);
+}
+
+void ClearInput() { // Clears buffer
+	cin.clear();
+	cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Ignores everything up to end of line.
+}
+
+void Pathfind(NodeMap &map) {
+	map.StartNode->ScanPath(map);
+	map.DisplayMap();
+	
+	if (*map.found == true) {
+		cout << "Path drawn! type anything to continue: ";
+	}
+	else {
+		cout << "No path found. type anything to continue: ";
+	}
+	ClearInput();
+	cin.get();
+	TurnPathIntoWall(map);
+	ClearInput();
+	map.DisplayMap();
+}
+
+void TurnPathIntoWall(NodeMap &map) {
+	for (auto &node : map.PathDrawn) {
+		node->SetIcon('X');
+		node->SetNodeType(3);
+		node->ResetNode();
+	}
+	*map.found = false;
+	map.StartNode = 0;
+	map.EndNode = 0;
+	map.PathDrawn.clear();
+}
