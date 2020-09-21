cce599927e97e4d5eb4f3d6b4efdda6fd198ef72
ASVegaWorks <67890025+ASVegaWorks@users.noreply.github.com>
2020-09-21 14:25:08 -0600

    Node.h
    The Node class contains data for each individual node in the map such as their node type, distances, pointers to the nodes around them, their coordinates, etc.

--- Node.h
+++ Node.h
@@ -1,10 +1,9 @@
 #pragma once
 #include"NodeMap.h"
 #include<iostream>
-#include<vector>
 #include<map>
 #include<memory>
-#include <thread>
+#include<queue>
 
 using namespace std;
 
@@ -21,26 +20,27 @@ public:
 	char GetIcon()const;
 	void SetIcon(char icon);
 	void SetNodeType(size_t nodeType);
-	void CalcDistances(const shared_ptr<Node> &endNode);
-	void ScanAdjacents(map<int, map<int, shared_ptr<Node>>> &mapContainer);
-	void ScanPath(NodeMap &map);
+	void StartScan(NodeMap &map);
 	void ResetNode();
 	int GetDistanceCost()const;
-	Node& operator=(const Node &rhs); // assignment operator overloaded
 	pair<int,int> &GetNodeCoords();
 	size_t GetNodeType()const;
 	vector<shared_ptr<Node>> AdjacentNodes;
-	vector<shared_ptr<Node>> DiagonalAdjacentNodes;
-	bool pathScanned = false;
-	bool isDiagonal = false;
+	Node* ParentNode = 0;
+
+
 private:
-	int distanceCost = 0;
-	int distanceToEnd = 0;
-	int distanceToStart = 0;
+	void ScanAdjacents(map<int, map<int, shared_ptr<Node>>> &mapContainer);
+	void CalcDistances(const NodeMap &map);
+	void RecursiveScan(NodeMap &map);
+	bool isInOpenList = false;
+	bool nodeVisited = false;
+	bool isDiagonal = false;
+	int distanceCost = 0; // F cost
+	int distanceToEnd = 0; // H cost
+	int distanceToStart = 0; //  G cost
+	int nodeType;
 	char icon;
 	pair<int, int> locationCoords;
-	int nodeType;
 };
-
-bool operator<(const shared_ptr<Node>lhs, const shared_ptr<Node> rhs);
 
