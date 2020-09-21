16eb9c8331abb576f45da5e518c83b82dee325b7
ASVegaWorks <67890025+ASVegaWorks@users.noreply.github.com>
2020-09-21 14:19:34 -0600

    NodeMap.h
    NodeMap is a class containing data such as the entire 2D grid map of nodes, the latest path draw, and the latest start and end node.

--- NodeMap.h
+++ NodeMap.h
@@ -2,28 +2,44 @@
 #include"Node.h"
 #include<iostream>
 #include<map>
-#include<set>
 #include<memory>
 #include<iomanip>
+#include<vector>
+#include<queue>
 using namespace std;
 
 class Node;
 
+template<class T>
+struct PrioQueueAscending {
+	bool operator()(const T &lhs, const T &rhs)const {
+		return (lhs->GetDistanceCost() > rhs->GetDistanceCost());
+	}
+};
+
 class NodeMap
 {
 public:
+
 	map<int, map<int, shared_ptr<Node>>> NodeMapContainer;
-	shared_ptr<bool> found = make_unique<bool>(false);
-	set<shared_ptr<Node>> PathDrawn;
+	vector<shared_ptr<Node>> VisitedNodes;
+	vector<Node*> Path;
+	priority_queue<shared_ptr<Node>,vector<shared_ptr<Node>>, PrioQueueAscending<shared_ptr<Node>>> ScannedNodes;
+	shared_ptr<bool> endNodeFound = make_unique<bool>(false);
+	shared_ptr<bool> startNodeFound = make_unique<bool>(false);
 	shared_ptr<Node> StartNode;
 	shared_ptr<Node> EndNode;
 	void DisplayMap()const;
+	void ClearInput();
 	static NodeMap& GetInstance(size_t size); // ensures only one instance possible
 	void PlaceNode(shared_ptr<Node> &nodeToPlace);
-	void ScanNodeAdjacents();
+	void TracePath();
+	void Pathfind();
+	void TurnPathIntoWall();
+	void TestMap();
+
 private:
 	NodeMap(const size_t size);
 	~NodeMap();
 	const size_t MapSize;
 };
-
